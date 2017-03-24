a10fc4f6f4a8c7d2ec67edfb4929cefbc854b977
teng-shipc <964378878@qq.com>
2017-03-24 08:57:58 +0800

    爬虫login修改成功

--- daily/DoubanSpider.py
+++ daily/DoubanSpider.py
@@ -4,15 +4,13 @@ import requests
 
 
 if __name__ == '__main__':
-    login_data = {'email': '964378878@qq.com',
-                  'password': 'sbjjbs10517'}
+    login_data = {'form_email': '964378878@qq.com',
+                  'form_password': 'sbjjbs10517'}
 
-    login_url = 'https://www.douban.com/accounts/login'
+    login_url = 'https://accounts.douban.com/login'
     someone_url = 'https://www.douban.com/people/er-mao/contacts'
 
-    loginRequest = requests.post(login_url, login_data)
-    print loginRequest.status_code
-
-    jar = requests.cookies.RequestsCookieJar()
-    r = requests.get(someone_url, cookies=jar)
-    print r.text
+    with requests.Session() as session:
+        session.post(login_url, login_data)
+        r = session.get(someone_url)
+        print r.text
