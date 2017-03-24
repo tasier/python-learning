6180d8524f199a6e85d83eee045bc96d7f475b52
teng-shipc <964378878@qq.com>
2017-03-24 09:01:49 +0800

    爬虫login修改成功-v2

--- daily/DoubanSpider.py
+++ daily/DoubanSpider.py
@@ -4,8 +4,8 @@ import requests
 
 
 if __name__ == '__main__':
-    login_data = {'form_email': '964378878@qq.com',
-                  'form_password': 'sbjjbs10517'}
+    login_data = {'form_email': '',
+                  'form_password': ''}
 
     login_url = 'https://accounts.douban.com/login'
     someone_url = 'https://www.douban.com/people/er-mao/contacts'
